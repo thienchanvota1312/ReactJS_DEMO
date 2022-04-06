f51a78366d5a0a232c1bfceff6c6199f2071b216
thienchanvota1312 <thienchanvota1312@gmail.com>
2022-04-06 15:30:16 +0700

    MF

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "font-awesome/css/font-awesome.css";
+import { FOLLOW, HELP, HOME, TYPE } from "../constants/constan";
 
 function Footer() {
   return (
@@ -8,7 +9,7 @@ function Footer() {
   	 	<div className="row">
   	 		<div className="col">
   	 			<ul>
-                    <li><a href="home"><h3>Home</h3></a></li>
+                    <li><a href="home"><h3>{HOME}</h3></a></li>
   	 				<li><a href="#">about us</a></li>
   	 				<li><a href="#">contact</a></li>
   	 				<li><a href="#">copy right</a></li>
@@ -16,7 +17,7 @@ function Footer() {
   	 		</div>
   	 		<div className="col">
   	 			<ul>
-                    <h3>Help</h3>
+                    <h3>{HELP}</h3>
   	 				<li><a href="#">question</a></li>
   	 				<li><a href="#">provition</a></li>
   	 				<li><a href="#">info</a></li>
@@ -25,7 +26,7 @@ function Footer() {
   	 		
   	 		<div className="col">
   	 			<ul>
-                    <h3>Type</h3>
+                    <h3>{TYPE}</h3>
   	 				<li><a href="movie">movie</a></li>
   	 				<li><a href="series">series</a></li>
   	 				<li><a href="game">game</a></li>
@@ -33,7 +34,7 @@ function Footer() {
   	 		</div>
   	 		<div className="col">
                 <ul>
-  	 			<h3>Follow</h3>
+  	 			<h3>{FOLLOW}</h3>
   	 			<div className="social-links">
   	 				<a href="#"><i className="fa fa-facebook"></i></a>
   	 				<a href="#"><i className="fa fa-twitter"></i></a>
--- src/constants/constan.js
+++ src/constants/constan.js
@@ -2,4 +2,7 @@ export const SERIES = 'Series';
 export const HOME = 'Home';
 export const MOVIE = 'Movie';
 export const EPISODE = 'Game';
+export const HELP = 'Help';
+export const TYPE = 'Type';
+export const FOLLOW = 'Follow';
 
